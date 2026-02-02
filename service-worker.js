0e9d2cfb67aabee2f8c0bcd5d7beaad2a6140b4b
BroccoliSurprise <61516592+BroccoliSurprise@users.noreply.github.com>
2026-02-02 22:49:09 +0100

    Update service-worker.js
    Added offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,21 @@
+const CACHE_NAME = 'bitbot-cache-v1';
+const FILES_TO_CACHE = [
+  './bitbotremote.html',
+  './facerace.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(FILES_TO_CACHE)
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() =>
-      new Response('Offline', {
-        headers: { 'Content-Type': 'text/plain' }
-      })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
     )
   );
 });
